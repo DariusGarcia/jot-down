b59c20f8e65ae74e1ee7f57d8fc5c3af4aa897f1
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-12 22:55:22 -0800

    add logic to start server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+// import routes
+const api = require('./routes/api/api')
+const defaultRoutes = require('./routes/default/defaultRoutes')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use('/api', api)
+app.use('/', defaultRoutes)
+
+// start express app
+app.listen(PORT, () =>
+	console.log(`Server started. Listening on PORT: ${PORT}`)
+)
