71db7b9c91e7b6c63ed9b15e228d7c6c0b0d1a0f
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-14 21:42:42 -0800

    added get route in api router

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,28 +1,14 @@
 const router = require('express').Router()
+const util = require('../../utils/utils-db')
 
-// connecting sql db
-// const db = mysql.createConnection(
-// 	{
-// 		host: 'localHost',
-// 		user: 'root',
-// 		password: 'Slugger132435!',
-// 		database: 'users_db',
-// 	},
-// 	console.log(`Connected to the users_db database.`)
-// )
-
-router.get('/users', (req, res) => {
-	res.json('hello there').sendStatus(200)
+router.get('/notes', (req, res) => {
+	util.fetchData().then((data) => {
+		return res.json(data)
+	})
 })
 
 router.post('/users', (req, res) => {
 	res.json(req.body)
 })
 
-// app.get('/api/users', (req, res) => {
-// 	db.query('SELECT * FROM users', function (err, results) {
-// 		if (err) throw err
-// 		res.json(results)
-// 	})
-// })
 module.exports = router
